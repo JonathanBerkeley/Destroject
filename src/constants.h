bc154229f1f59a0e246cf7aedea16b7eb5f5f0fd
Jonathan Berkeley <jonathanberkeley1999@gmail.com>
2021-06-07 23:41:28 +0100

    Update version constant

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,5 @@
 #pragma once
 #include <string>
-const std::string VERSION = "0.1.1";
+const std::string VERSION = "0.1.2";
 const std::string TARGET = "Brawlhalla";
 const std::wstring WTARGET = std::wstring(TARGET.begin(), TARGET.end());
